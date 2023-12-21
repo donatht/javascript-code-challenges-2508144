eb228b0acdf2dfa73582085c5f1462647ab63cb1
donatht <107429042+donatht@users.noreply.github.com>
2023-12-21 12:57:51 +0000

    12 erfolgreich

--- challenges/12openorclosed.js
+++ challenges/12openorclosed.js
@@ -1,6 +1,13 @@
 function isOpen(timestamp) {
   // Write your code here
-
+  const weekDay = new Date(timestamp).getDay();
+  const hour = new Date(timestamp).getHours();
+  console.log(weekDay, hour)
+  if ((((weekDay == 1) || (weekDay == 2) || (weekDay == 4) || (weekDay == 5)) && ((hour >= 8 && hour <= 12) || (hour >= 14 && hour <= 18))) || ((weekDay == 3) && (hour >= 8 && hour <= 12)) || ((weekDay == 6) && (hour >= 8 && hour <= 13))) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 
