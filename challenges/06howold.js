e8a79e7a8aa42b4044e9d3c4fa5dff532534f8b3
donatht <107429042+donatht@users.noreply.github.com>
2023-12-21 09:45:21 +0000

    06 erfolgreich

--- challenges/06howold.js
+++ challenges/06howold.js
@@ -1,5 +1,11 @@
 function howOld(dates) {
   // Write your code here
+  dates = dates.map(Date.parse);
+  let years = new Date(Math.abs(dates[0]-dates[1])).getFullYear()-1970;
+  return years;
+
+
+  //return Math.abs(dates[0].getTime() - dates[1].getTime());
 
 }
 
@@ -10,6 +16,6 @@ describe("Tests", () => {
     assert.equal(howOld(['1. January 2025', '31 December 2024']), 0);
     assert.equal(howOld(['31 December 2024', '1. January 2025']), 0);
     assert.equal(howOld(['12 May 2024', '11 May 2034']), 9);
-    assert.equal(howOld(['12 May 2024', '12 May 2034']), 10);  
+    assert.equal(howOld(['12 May 2024', '12 May 2034']), 10);
   });
 });
