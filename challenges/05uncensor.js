f776edbc886ff5bee4bc24ab7b4a47ab0883ade1
donatht <107429042+donatht@users.noreply.github.com>
2023-12-21 09:14:43 +0000

    05 erfolgreich

--- challenges/05uncensor.js
+++ challenges/05uncensor.js
@@ -1,11 +1,16 @@
 function uncensor(infected, remedy) {
   // Write your code here
+  let key = [...remedy]
+  while (infected.includes("*")) {
+    infected = infected.replace("*", key.shift());
+  }
 
+  return infected
 }
 
 
-const { assert } = require('chai'); 
-describe("Tests", function() {
+const { assert } = require('chai');
+describe("Tests", function () {
   it("tests", () => {
     assert.deepEqual(uncensor('H*l** W*l*', 'aloet'), 'Hallo Welt');
     assert.deepEqual(uncensor('G*h*im*r T*xt', 'eeee'), 'Geheimer Text');
